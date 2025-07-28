aa7ec9125d53cf391dbecee0b384aaa0d73cd844
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-27 23:36:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
+# Loading APP Modules
+from Modules.Utils.header import show_header
 from Modules.Utils.load_data import load_dataset
 
+# Loading Data
 df = load_dataset("Data/Yelp_sampled_df.parquet")
+
+#GUI
+show_header("🧠 Business Intelligence Explorer")
+
 st.dataframe(df.head())

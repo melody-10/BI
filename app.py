4430c11d3e5f8a3f12139ba47b5c02cae0e9c137
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-27 23:41:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
 # Loading APP Modules
+from Modules.UI.layout_config import set_layout
 from Modules.UI.header import show_header
 from Modules.Utils.load_data import load_dataset
+
+# Layout
+set_layout()
 
 # Loading Data
 df = load_dataset("Data/Yelp_sampled_df.parquet")

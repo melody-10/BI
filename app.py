176beb37b066173a330c0506b97f59c1fae76f12
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-28 06:39:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ with col2:
 filtered_df = filter_data(df, state=selected_state, categories=selected_categories)
 
 # Show filtered data
-st.markdown(f"### Showing results for `{selected_state}` with selected categories: {} Total number of Observations".format(len(filtered_df))
+st.markdown(f"### Showing results for `{selected_state}` with selected categories: {len(filtered_df)} Total number of Observations")
 st.dataframe(filtered_df.head(10))

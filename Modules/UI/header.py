2f540e1df1e48657f2585943643003694658981a
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-28 06:57:22 -0500

    Update header.py

--- Modules/UI/header.py
+++ Modules/UI/header.py
@@ -7,7 +7,7 @@ def show_header(text_title: str):
     col1, col2 = st.columns([1, 6])
     
     with col1:
-        st.image("assets/UP logo.jpg", width=80)
+        st.image("assets/UP logo.jpg", width=100)
         
     with col2:
         st.title(text_title)
